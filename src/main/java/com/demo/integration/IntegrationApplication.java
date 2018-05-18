4a1db9756247c14cffc4be4eb312108083bf056c
li <1282471835@qq.com>
2018-05-18 10:23:55 +0800

    整合redis

--- src/main/java/com/demo/integration/IntegrationApplication.java
+++ src/main/java/com/demo/integration/IntegrationApplication.java
@@ -2,8 +2,10 @@ package com.demo.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class IntegrationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/demo/integration/login/service/impl/LoginService.java
+++ src/main/java/com/demo/integration/login/service/impl/LoginService.java
@@ -1,6 +1,10 @@
 package com.demo.integration.login.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.demo.integration.login.entity.Department;
@@ -8,29 +12,34 @@ import com.demo.integration.login.mapper.LoginMapper;
 import com.demo.integration.login.service.ILoginService;
 
 @Service
+@CacheConfig(cacheNames="loginService")
 public class LoginService implements ILoginService{
     
     @Autowired
     private LoginMapper loginMapper;
     
+    @CachePut(key="#department.id")
     public Department save(Department department){
         System.out.println("保存 id=" + department.getId() + " 的数据");
         loginMapper.insert(department);
         return department;
     }
     
+    @CachePut(key="#department.id")
     public Department update(Department department) {
         System.out.println("修改 id=" + department.getId() + " 的数据");
         loginMapper.update(department);
         return department;
     } 
     
+    @Cacheable(key="#id")
     public Department getDepartmentById(Integer id) {
         System.out.println("获取 id=" + id + " 的数据");
         Department department = loginMapper.getById(id);
         return department;
     }
     
+    @CacheEvict(key="#id")
     public void delete(Integer id) {
         System.out.println("删除 id=" + id + " 的数据");
         loginMapper.deleteById(id);
