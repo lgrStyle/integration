07082319480ea581fe0316fd3aee4c5565cef0f0
li <1282471835@qq.com>
2018-06-06 10:56:59 +0800

    增加工作流模块

---
+++ src/main/java/com/demo/integration/workflow/service/impl/WorkflowService.java
@@ -0,0 +1,12 @@
+package com.demo.integration.workflow.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.demo.integration.workflow.service.IWorkflowService;
+
+@Service
+@Transactional
+public class WorkflowService implements IWorkflowService{
+
+}
