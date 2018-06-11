9b2325c6e7d515459c60b478988764aa8674d245
li <1282471835@qq.com>
2018-06-11 15:33:54 +0800

    增加工作流测试

--- src/test/java/com/demo/integration/IntegrationApplicationTests.java
+++ src/test/java/com/demo/integration/IntegrationApplicationTests.java
@@ -1,10 +1,16 @@
 package com.demo.integration;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.zip.ZipInputStream;
 
 import org.activiti.engine.RepositoryService;
-import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.repository.Deployment;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +25,41 @@ public class IntegrationApplicationTests {
     private RepositoryService repositoryService;
     
 	@Test
-	public void contextLoads() {
-	   ProcessDefinition processDefinition =  repositoryService.getProcessDefinition("test");
-	   assertNull(processDefinition);
+	public void contextLoads() throws FileNotFoundException {
+	    long count = repositoryService.createDeploymentQuery()
+	        .processDefinitionKey("testProcess")
+	        .count();
+	    assertEquals(count, 1);
 	}
 	
-	@Test
-	public void notNullTest() {
-	    assertNotNull(repositoryService);
+	public void addResource() {
+	    InputStream bpmn = IntegrationApplicationTests.class.getClassLoader().getResourceAsStream("processes/testProcess.bpmn");
+        InputStream png = IntegrationApplicationTests.class.getClassLoader().getResourceAsStream("processes/testProcess.png");
+        repositoryService.createDeployment()
+            .name("测试")
+            .addInputStream("bpmn", bpmn)
+            .addInputStream("png", png)
+            .deploy();
 	}
-
+	
+	@Before
+	public void addZipResource() {
+	    IntegrationApplicationTests.class.getClassLoader();
+        InputStream in = ClassLoader.getSystemResourceAsStream("processes/test.zip");
+	    ZipInputStream zip = new ZipInputStream(in);
+	    repositoryService.createDeployment()
+	        .name("zip测试")
+	        .addZipInputStream(zip)
+	        .deploy();
+	}
+	
+	@After
+	public void deleteResource() {
+	    List<Deployment> list = repositoryService.createDeploymentQuery()
+	            .processDefinitionKey("testProcess")
+	            .list();
+	    for(Deployment deployment : list) {
+	        repositoryService.deleteDeployment(deployment.getId(), true);
+	    }
+	}
 }
