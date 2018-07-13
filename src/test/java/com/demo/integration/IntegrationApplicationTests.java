826aeeb6993452ab7523db487f40c41c08846925
li <1282471835@qq.com>
2018-07-13 11:46:28 +0800

    增加流程

--- src/test/java/com/demo/integration/IntegrationApplicationTests.java
+++ src/test/java/com/demo/integration/IntegrationApplicationTests.java
@@ -1,6 +1,29 @@
 package com.demo.integration;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.FormService;
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.history.HistoricDetail;
+import org.activiti.engine.history.HistoricFormProperty;
+import org.activiti.engine.history.HistoricProcessInstance;
+import org.activiti.engine.history.HistoricVariableInstance;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -8,4 +31,79 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class IntegrationApplicationTests {
 
+    @Autowired
+    private IdentityService identityService;
+    
+    @Autowired
+    private RepositoryService repositoryService;
+    
+    @Autowired
+    private FormService formService;
+    
+    @Autowired
+    private TaskService taskService;
+    
+    @Autowired
+    private HistoryService historyService;
+    
+    @Test
+//    @Transactional
+    public void allApproved() {
+        String currentUser = "ming";
+        identityService.setAuthenticatedUserId(currentUser);
+        
+        ProcessDefinition pd = repositoryService.createProcessDefinitionQuery().processDefinitionKey("leaveProcess").singleResult();
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Map<String,String> variables = new HashMap<>();
+        Calendar calendar = Calendar.getInstance();
+        String startDate = sdf.format(calendar.getTime());
+        calendar.add(Calendar.DAY_OF_MONTH, 2);
+        String endDate = sdf.format(calendar.getTime());
+        variables.put("startDate", startDate);
+        variables.put("endDate", endDate);
+        variables.put("reason", "调休");
+        
+        ProcessInstance pi = formService.submitStartFormData(pd.getId(), variables);
+        assertNotNull(pi);
+        
+        Task deptTask = taskService.createTaskQuery().taskCandidateGroup("deptLeader").singleResult();
+        variables = new HashMap<>();
+        variables.put("deptLeaderApprove", "true");
+        formService.submitTaskFormData(deptTask.getId(), variables);
+        
+        Task hrTask = taskService.createTaskQuery().taskCandidateGroup("hr").singleResult();
+        variables = new HashMap<>();
+        variables.put("hrApprove", "true");
+        formService.submitTaskFormData(hrTask.getId(), variables);
+        
+        Task reportBackTask = taskService.createTaskQuery().taskAssignee(currentUser).singleResult();
+        variables = new HashMap<>();
+        variables.put("reportBackDate", "2018-07-20");
+        formService.submitTaskFormData(reportBackTask.getId(), variables);
+        
+        HistoricProcessInstance hpi =  historyService.createHistoricProcessInstanceQuery().finished().singleResult();
+        assertNotNull(hpi);
+        
+        Map<String,Object> historyVariables = packageVariables(pi);
+        assertEquals("ok", historyVariables.get("result"));
+    }
+    
+    private Map<String,Object> packageVariables(ProcessInstance pi){
+        Map<String,Object> variables = new HashMap<>();
+        List<HistoricDetail> list = historyService.createHistoricDetailQuery().processInstanceId(pi.getProcessInstanceId()).list();
+        List<HistoricVariableInstance> varList = historyService.createHistoricVariableInstanceQuery().processInstanceId(pi.getProcessInstanceId()).list();
+        for(HistoricVariableInstance var : varList) {
+            variables.put(var.getVariableName(),var.getValue());
+            System.out.println("variable: "+var.getVariableName()+"="+var.getValue());
+        }
+        for(HistoricDetail historicDetail : list) {
+            if(historicDetail instanceof HistoricFormProperty) {
+                HistoricFormProperty field = (HistoricFormProperty) historicDetail;
+                variables.put(field.getPropertyId(), field.getPropertyValue());
+                System.out.println("form field: taskId="+field.getTaskId()+","+field.getPropertyId()+"="+field.getPropertyValue());
+            }
+        }
+        return variables;
+    }
 }
