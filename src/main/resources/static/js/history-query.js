9082ffc977badad36ec395f887297e1a4b634e7c
li <1282471835@qq.com>
2018-07-24 15:42:51 +0800

    修改历史查询

--- src/main/resources/static/js/history-query.js
+++ src/main/resources/static/js/history-query.js
@@ -28,7 +28,7 @@ $(function(){
 	    {title:'所属流程',field:'processName',width:200,align:'center'},
 	    {title:'流程发起人',field:'createBy',width:100,align:'center'},
 	    {title:'开始时间',field:'createTime',width:150,align:'center'},
-	    {title:'步骤与流程图',field:'taskName',width:150,align:'center'},
+	    {title:'当前步骤',field:'taskName',width:150,align:'center'},
 	    {title:'状态',field:'status',width:100,align:'center'},
 	    {title:'公共附件',field:'attachment',width:100,align:'center'},
 	    {title:'操作',field:'operation',width:150,align:'center'},
