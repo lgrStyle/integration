46713c7356a3a123c876d0d029e7714186081eaf
li <1282471835@qq.com>
2018-09-28 16:13:05 +0800

    修改启动方式

--- src/main/java/com/demo/integration/IntegrationApplication.java
+++ src/main/java/com/demo/integration/IntegrationApplication.java
@@ -1,17 +1,32 @@
 package com.demo.integration;
 
 import org.activiti.spring.boot.SecurityAutoConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableCaching
 @EnableTransactionManagement
-public class IntegrationApplication {
-
+public class IntegrationApplication extends SpringBootServletInitializer{
+    
+    private static Logger logger = LoggerFactory.getLogger(IntegrationApplication.class);
+    
 	public static void main(String[] args) {
 		SpringApplication.run(IntegrationApplication.class, args);
+		logger.info("jar方式启动！");
 	}
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        logger.info("war方式启动！");
+        return builder.sources(IntegrationApplication.class);
+    }
+	
+	
 }
